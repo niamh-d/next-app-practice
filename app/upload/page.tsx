11923022217c5c0869cac1d4c0d99a1879509e16
Niamh <niamh.linguist@gmail.com>
2023-11-28 23:43:19 +0200

    customised upload widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -6,6 +6,31 @@ interface CloudinaryResult {
   public_id: string;
 }
 
+const uWStyles = {
+  palette: {
+    window: "#F5F5F5",
+    sourceBg: "#FFFFFF",
+    windowBorder: "#90a0b3",
+    tabIcon: "#0094c7",
+    inactiveTabIcon: "#69778A",
+    menuIcons: "#0094C7",
+    link: "#53ad9d",
+    action: "#8F5DA5",
+    inProgress: "#0194c7",
+    complete: "#53ad9d",
+    error: "#c43737",
+    textDark: "#000000",
+    textLight: "#FFFFFF",
+  },
+  fonts: {
+    default: null,
+    "'Fira Sans', sans-serif": {
+      url: "https://fonts.googleapis.com/css?family=Fira+Sans",
+      active: true,
+    },
+  },
+};
+
 const UploadPage = () => {
   const [publicId, setPublicId] = useState("");
   return (
@@ -20,7 +45,14 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="jdimrdn3"
-        onUpload={(result, widget) => {
+        options={{
+          sources: ["local", "url", "camera", "google_drive"],
+          cropping: true,
+          multiple: false,
+          maxFiles: 5,
+          styles: uWStyles,
+        }}
+        onUpload={(result) => {
           if (result.event !== "success") return;
           const info = result.info as CloudinaryResult;
           setPublicId(info.public_id);
