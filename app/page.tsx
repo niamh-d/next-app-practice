f80900e82beb65a53532480190212a56f25a3bf7
Niamh <niamh.linguist@gmail.com>
2024-01-05 19:19:29 +0200

    logic to sign user out

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>Hello, {session && <span>{session.user!.name}</span>}!</h1>
+      <h1>Hello{session && <span>, {session.user!.name}</span>}!</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
